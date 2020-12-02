52fb98df9beb7db724b0a9a2b2fd698d6f66ce53
pregula <pawel.regula92@gmail.com>
2020-12-02 14:41:32 +0100

    Added exception types.

--- SelfHost/Framework/ErrorHandlerMiddleware.cs
+++ SelfHost/Framework/ErrorHandlerMiddleware.cs
@@ -29,7 +29,21 @@ namespace SelfHost.Framework
 
         private static Task HandleErrorAsync(HttpContext context, Exception exception)
         {
+            var exceptionType = exception.GetType();
             var statusCode = HttpStatusCode.InternalServerError;
+            switch (exception)
+            {
+                case Exception e when exceptionType == typeof(UnauthorizedAccessException):
+                    statusCode = HttpStatusCode.Unauthorized;
+                    break;
+
+                case Exception e when exceptionType == typeof(ArgumentException):
+                    statusCode = HttpStatusCode.BadRequest;
+                    break;
+
+                default:
+                    break;
+            }
             var response = new { message = exception.Message };
             var payload = JsonConvert.SerializeObject(response);
             context.Response.ContentType = "application/json";
